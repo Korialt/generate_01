efd3f93a1541e2c4e29640c63574dc3ced2f01f3
Korialt <dentriffemai@gmail.com>
2024-11-15 08:33:02 +0800

    add file writer to text file as permenantly recordings.

--- generator-demo-projects/acm-template/src/com/dent/acm/Main.java
+++ generator-demo-projects/acm-template/src/com/dent/acm/Main.java
@@ -1,21 +1,37 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static int depth = 0;
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //"C:/Users/Dent/dentcode/"
-        File file = new File("./D.txt");
+        /*File file = new File("./D.txt");
         if (!file.exists()) {
             System.out.println("File not found");
 
+        }*/
+        boolean flag = false;
+        String path = args[0];
+        String output = args[0] + "1" + ".txt";
+        File file = new File(output);
+        if (!file.exists()) {
+            flag = file.createNewFile();
         }
-        //String path = "D:/";
-        String path = System.getProperty("user.dir");
-        scan(path);
+        if (flag) {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+            //System.getProperty("user.dir");
+            //"D:/";
+            //String path = System.getProperty("user.dir");
+            scan(path, writer);
+            writer.close();
+        }
+
 
         /*
         Scanner input = new Scanner(System.in);
@@ -39,8 +55,11 @@ public class Main {
         return a + 1;
     }
 
-    public static void scan(String path) {
+    public static void scan(String path, BufferedWriter writer) throws IOException {
         File file = new File(path);
+        if (!file.exists()) {
+            System.out.println("No such a file!");
+        }
         File[] files = file.listFiles();
         if (files != null) {
             for (File f : files) {
@@ -48,15 +67,20 @@ public class Main {
                     ++ depth;
                     for (int i = 0; i < depth - 1; ++ i){
                         System.out.print("\t");
+                        writer.write("\t");
+
                     }
                     System.out.println(f.getName() + ":");
-                    scan(f.getPath());
+                    writer.write(f.getName() + ":" + "\n");
+                    scan(f.getPath(), writer);
                     depth--;
                 } else {
                     for (int i = 0; i < depth; ++ i){
                         System.out.print("\t");
+                        writer.write("\t");
                     }
-                    System.out.println(f.getName());
+                    System.out.println(f.getName() + " " + f.length() + " Byte(s) " + f.lastModified());
+                    writer.write(f.getName() + " " + f.length() + " Byte(s)" + "\n");
                 }
             }
         }
